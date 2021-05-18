11c724bf73af4fc0b8e8f6e77d043f4821728927
codewithmaddy <71321288+codewithmaddy@users.noreply.github.com>
2021-05-18 10:08:39 +0530

    About section done

--- src/components/landingPage/About.js
+++ src/components/landingPage/About.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React from "react";
+import  about_img from "../static/About.png"
+import {Button} from "@material-ui/core";
 import "./css/About.css";
 
 function About() {
-    return (
-        <div>
-            <h1>About</h1>
+  return (
+    <div className="About">
+      <div className="About_left">
+        <div className="About_left_main">
+          <p>GetWell where Healing, Teaching, Discovery come together.</p>
         </div>
-    )
+        <div className="About_left_sub">
+          <p>Don't Spend thousands on doctor consultation</p>
+        </div>
+        <div className="About_left_sub_2">
+          <p>
+            our passionate and verified doctors will do it for free or for 1% of
+            the cost.
+          </p>
+        </div>
+        <Button variant="contained" color="primary">
+            Consult Now
+        </Button>
+      </div>
+      <div className="About_right">
+          <img src={about_img}></img>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
