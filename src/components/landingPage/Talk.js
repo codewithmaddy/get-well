266c19a64649ce6f2a501aa019b178d2618865b1
codewithmaddy <71321288+codewithmaddy@users.noreply.github.com>
2021-05-18 10:59:44 +0530

    Talk section done

--- src/components/landingPage/Talk.js
+++ src/components/landingPage/Talk.js
@@ -1,12 +1,39 @@
-import React from 'react'
-import "./css/Talk.css"
+import React from "react";
+import "./css/Talk.css";
+import  talk_1_img from "../static/talk_1.jpg"
+import  talk_2_img from "../static/talk_2.jpg"
+
 
 function Talk() {
-    return (
-        <div>
-            <h1>Talk section</h1>
+  return (
+    <div className="Talk">
+      <h1>Talk to a..</h1>
+      <div className="Talk_card">
+        <div class="card card-1">
+          <div class="top">
+            <img
+              src={talk_1_img}
+              alt="material ui"
+            />
+          </div>
+          <div class="bottom">
+            <p>Covid Recovered Person</p>
+          </div>
         </div>
-    )
+        <div class="card card-1">
+          <div class="top">
+            <img
+              src={talk_2_img}
+              alt="material ui"
+            />
+          </div>
+          <div class="bottom">
+            <p>Stranger</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Talk
+export default Talk;
